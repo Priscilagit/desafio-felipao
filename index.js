d9736cb2bfff2cf5fb63fccaf2dd61b58fe47e23
Priscila <prioolivesouza@hotmail.com>
2023-11-24 20:12:19 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Olá Munndão")
+let nomeHeroi = "Venus"
+let xp = 2000 
+
+
+if (xp <= 1000){
+    console.log(nomeHeroi + " tem um Xp com categoria bronze.")
+}else if (xp === 2000){
+    console.log(nomeHeroi + " tem um Xp com categoria prata.")
+}else {
+    console.log("Seu héroi não entra em nenhuma categoria")
+}
